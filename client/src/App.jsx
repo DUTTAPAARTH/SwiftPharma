d53de75e010f1e4de6dcabc51375a84cc52f8155
yash-pratap-create <robinhoodyash15@gmail.com>
2026-06-21 01:40:38 +0530

    feat: implement singleton Socket.IO context to eliminate connection spam
    BREAKING CHANGE: Refactored Socket.IO to use shared context pattern
    
    - Created SocketContext with single shared connection
    - All components now use ONE socket instead of creating their own
    - useEmergencySocket refactored to use shared socket
    - DeliveryDashboard updated to use shared socket
    - Wrapped App with SocketProvider
    - Fixes 'Session ID unknown' errors from multiple connections
    - Eliminates rapid connect/disconnect spam in logs
    - Significantly reduces server load and connection overhead
    
    Before: 5-10 simultaneous connections per user
    After: 1 connection per user
    
    This fixes the root cause of Socket.IO instability

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import RoutesConfig from "./routes";
 import { AuthProvider } from "./context/AuthContext";
+import { SocketProvider } from "./context/SocketContext";
 import { CartProvider } from "./context/CartContext";
 import { ThemeProvider } from "./context/ThemeContext";
 import { PrescriptionProvider } from "./context/PrescriptionContext";
@@ -11,19 +12,21 @@ import DoseAlertBanner from "./components/DoseAlertBanner";
 const App = () => {
   return (
     <AuthProvider>
-      <PrescriptionProvider>
-        <CartProvider>
-          <WishlistProvider>
-            <ThemeProvider>
-              <HealthCompanionProvider>
-                <DoseAlertBanner />
-                <RoutesConfig />
-                <SpeedInsights />
-              </HealthCompanionProvider>
-            </ThemeProvider>
-          </WishlistProvider>
-        </CartProvider>
-      </PrescriptionProvider>
+      <SocketProvider>
+        <PrescriptionProvider>
+          <CartProvider>
+            <WishlistProvider>
+              <ThemeProvider>
+                <HealthCompanionProvider>
+                  <DoseAlertBanner />
+                  <RoutesConfig />
+                  <SpeedInsights />
+                </HealthCompanionProvider>
+              </ThemeProvider>
+            </WishlistProvider>
+          </CartProvider>
+        </PrescriptionProvider>
+      </SocketProvider>
     </AuthProvider>
   );
 };
